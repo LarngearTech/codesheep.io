f509864bfba78fc9591ed4a28ad466b646c7ec01
Pongnarin Tawan <pongnarin.thawan@gmail.com>
2016-07-27 11:47:27 +0700

    test remove cp in package

--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
 var ghpages = require('gh-pages');
 var path = require('path');
- 
-ghpages.publish(path.join(__dirname, 'public'), function(err) { console.log(err) });
+
+ghpages.publish(path.join(__dirname, 'public'), function(err) { console.log(err) }); 
