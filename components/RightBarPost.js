2e17cae875901420b0a35ddb25ad6a8f3fdfb5ca
Pongnarin Tawan <pongnarin.thawan@gmail.com>
2016-08-01 16:52:38 +0700

    add btn share in post right bar

--- components/RightBarPost.js
+++ components/RightBarPost.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import RecentPosts from './RecentPosts'
 import PostRightBarShare from './PostRightBarShare'
 
+
 export default class RightBarPost extends Component {
   render () {
+
     return (
     <div className='right-bar-post'>
       <div className='frame-right-bar-post'>
