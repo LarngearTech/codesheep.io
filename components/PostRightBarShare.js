67ac3d07cdb84d101c7d7f2a85b1ee5eaf2c82d6
Pongnarin Tawan <pongnarin.thawan@gmail.com>
2016-08-01 17:19:30 +0700

    edit follow to guideline

--- components/PostRightBarShare.js
+++ components/PostRightBarShare.js
@@ -8,9 +8,11 @@ export default class PostRightBarShare extends Component {
     return (
     <div className='post-right-bar-share'>
       <FacebookShareButton url={`www.codesheep.io/${this.props.url}`}>
-        <div className="btn-group" style={{width: '100%', cursor: 'pointer'}}>
-          <a className='btn btn-primary disabled'><i className="fa fa-facebook" style={{width: '20%'}}></i></a>
-          <a className='btn btn-primary ' href='' style={{width: '80%'}}>Share</a>
+        <div className='btn-group' style={{width: '100%', cursor: 'pointer'}}>
+          <a className='btn btn-primary disabled'>
+            <i className='fa fa-facebook' style={{width: '20%'}}></i>
+          </a>
+          <a className='btn btn-primary' style={{width: '80%'}}>Share</a>
         </div>
       </FacebookShareButton>
     </div>
